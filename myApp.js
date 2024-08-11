16559b75f414b30a5b4fbb1143cacd2d38ca4e39
ovextemeer <ovextemeer@protonmail.com>
2024-08-11 15:12:37 +0700

    FCC Back End Development and APIs course

--- myApp.js
+++ myApp.js
@@ -1,39 +1,50 @@
+let bodyParser = require('body-parser');
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use('/public', express.static(__dirname + '/public'));
 
+app.use(function (req, res, next) {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+});
 
+/*
+app.get('/', function (req, res) {
+    res.send('Hello Express');
+});
+*/
 
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+});
 
+app.get('/json', function (req, res) {
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        res.json({ "message": "HELLO JSON" });
+    } else {
+        res.json({ "message": "Hello json" });
+    }
+});
 
+app.get('/now', function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function (req, res) {
+    res.json({ time: req.time });
+})
 
+app.get('/:word/echo', function (req, res) {
+    res.json({ echo: req.params.word });
+});
 
+app.route('/name').get(function (req, res) {
+    res.json({ name: `${req.query.first} ${req.query.last}` });
+}).post(function (req, res) {
+    res.json({ name: `${req.body.first} ${req.body.last}` });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
